c79ffe05c88d6d00e4aff6af9c3974a02ccaa6d5
강배준 <baejun_k@naver.com>
2023-02-14 23:51:59 +0900

    초기 커밋.

---
+++ HexToString/HexToString.cs
@@ -0,0 +1,22 @@
+﻿namespace Utils
+{
+	public static class HexToString
+	{
+		public static bool ConvertTo(string hexStr, out byte[]? bytes)
+		{
+			bytes = null;
+			if (null == hexStr)
+			{
+				return false;
+			}
+			int bytesLen = (int)Math.Ceiling(hexStr.Length * 0.5);
+			bytes = new byte[((bytesLen > 0) ? bytesLen : 1)];
+			for (int i = 0; i < bytesLen; ++i)
+			{
+				bytes[i] = Convert.ToByte(hexStr.Substring(i * 2, 2), 16);
+			}
+
+			return true;
+		}
+	}
+}
---
+++ Test/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Security.Cryptography;
+
+using Utils;
+
+
+class Program
+{
+	static public int Main(string[] args)
+	{
+		string test = "FFccCF";
+
+		if ( HexToString.ConvertTo(test, out byte[]? result))
+		{
+			for(int i = 0; i < result?.Length; ++i)
+			{
+				Console.WriteLine($"{result?[i]}");
+			}
+
+		}
+
+		return 0;
+	}
+}
